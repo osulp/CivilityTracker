d4a14337af80e0ad318f88705e6465281fd0a5de
Trey Terrell <trey.terrell@oregonstate.edu>
2014-06-27 16:01:42 -0700

    On the fly QR Code Generation

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,9 @@ Rails.application.routes.draw do
     end
   end
 
+  get 'civil_entries/qr/:serial' => Dragonfly.app.endpoint { |params, app|
+    app.generate(:qr, "http://#{Rails.application.config.action_mailer.default_url_options[:host]}/civil_entries/register/#{params[:serial]}")
+  }
+
   root :to => "home#index"
 end
