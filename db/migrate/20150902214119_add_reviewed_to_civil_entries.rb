5c77466be16f4019d7c6fb73abeed70446b4c4b4
luisgreg99 <luisram90_101@hotmail.com>
2015-09-02 14:44:30 -0700

    added default to reviewed column in civil entries model

--- db/migrate/20150902214119_add_reviewed_to_civil_entries.rb
+++ db/migrate/20150902214119_add_reviewed_to_civil_entries.rb
@@ -1,5 +1,5 @@
 class AddReviewedToCivilEntries < ActiveRecord::Migration
   def change
-    add_column :civil_entries, :reviewed, :boolean
+    add_column :civil_entries, :reviewed, :boolean, :default => false
   end
 end
