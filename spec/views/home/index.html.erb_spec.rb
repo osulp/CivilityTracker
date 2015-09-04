f95a55790908075728f8f13ba433b75f3aed2ec4
luisgreg99 <luisram90_101@hotmail.com>
2015-09-04 10:03:44 -0700

    removed feature spec and updated view spec for the admin panel

--- spec/views/home/index.html.erb_spec.rb
+++ spec/views/home/index.html.erb_spec.rb
@@ -13,26 +13,45 @@ RSpec.describe "home/index.html.erb" do
   before do
     allow(view).to receive(:current_user).and_return(user) if user
   end
+  context "When non-admin user" do
+    let(:user) do
+      User.create(
+        :email => 'user@example.com',
+        :password => 'admin123',
+        :password_confirmation => 'admin123',
+      )
+    end
+    it "should not display the admin panel" do
+      render
+      expect(rendered).not_to have_content("Admin Panel")
+    end
+  end
 
   context "When signed in as an admin" do
-    
-    context "When no reviewed entries exist" do
-      it "should display a message saying there are no civil entries at this time." do
-        assign(:civil_entries, [])
-
-        render
-
-        expect(rendered).to have_content("There are currently no entries at this time.")
-      end
+    it "should not display the admin panel" do
+      render
+      expect(rendered).to have_content("Admin Panel")
     end
-    context "When reviewed entries exist" do
-      it "should display those entries" do
-        assign(:civil_entries, [stubbed_model])
 
-        render
+  end
 
-        expect(rendered).to have_content("123 Place place")
-      end
+  context "When no reviewed entries exist" do
+    it "should display a message saying there are no civil entries at this time." do
+      assign(:civil_entries, [])
+
+      render
+
+      expect(rendered).to have_content("There are currently no entries at this time.")
+    end
+  end
+
+  context "When reviewed entries exist" do
+    it "should display those entries" do
+      assign(:civil_entries, [stubbed_model])
+
+      render
+
+      expect(rendered).to have_content("123 Place place")
     end
   end
 
