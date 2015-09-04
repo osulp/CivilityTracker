3ec39870465612e7c84ac5d6e47dc39b7c8ebfd0
luisgreg99 <luisram90_101@hotmail.com>
2015-09-03 18:53:35 -0700

    updated home view spec

--- spec/views/home/index.html.erb_spec.rb
+++ spec/views/home/index.html.erb_spec.rb
@@ -1,24 +1,41 @@
 require 'spec_helper'
 
 RSpec.describe "home/index.html.erb" do
-  context "When no reviewed entries exist" do
-    it "should display a message saying there are no civil entries at this time." do
-      assign(:civil_entries, [])
+  let(:user) do
+    User.create(
+      :email => 'user@example.com',
+      :password => 'admin123',
+      :password_confirmation => 'admin123',
+      :admin => true
+    )
+  end
 
-      render
+  before do
+    allow(view).to receive(:current_user).and_return(user) if user
+  end
 
-      expect(rendered).to have_content("There are currently no entries at this time.")
+  context "When signed in as an admin" do
+    
+    context "When no reviewed entries exist" do
+      it "should display a message saying there are no civil entries at this time." do
+        assign(:civil_entries, [])
+
+        render
+
+        expect(rendered).to have_content("There are currently no entries at this time.")
+      end
+    end
+    context "When reviewed entries exist" do
+      it "should display those entries" do
+        assign(:civil_entries, [stubbed_model])
+
+        render
+
+        expect(rendered).to have_content("123 Place place")
+      end
     end
   end
-  context "When reviewed entries exist" do
-    it "should display those entries" do
-      assign(:civil_entries, [stubbed_model])
 
-      render
-
-      expect(rendered).to have_content("123 Place place")
-    end
-  end
 end
 
 def stubbed_model
