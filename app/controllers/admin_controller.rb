dae3a1246a992c7e188a097ac8dde6221b4b36ef
luisgreg99 <luisram90_101@hotmail.com>
2015-09-03 18:39:47 -0700

    added controller and view spec

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,6 +1,16 @@
 class AdminController < ApplicationController
+  before_filter :authorize
   def index
 
+  end
+
+  private
+
+  def authorize
+    unless current_user && current_user.admin?
+      flash[:error] = "You do not have sufficient permissions to view this page"
+      redirect_to root_path
+    end
   end
 
 end
