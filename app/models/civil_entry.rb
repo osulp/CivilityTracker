353a706558af9a275a990203b970412185f9f525
Brandon Straley <straleyb@onid.oregonstate.edu>
2015-10-06 14:46:01 -0700

    Made geocoder save city, state and country

--- app/models/civil_entry.rb
+++ app/models/civil_entry.rb
@@ -1,6 +1,10 @@
 class CivilEntry < ActiveRecord::Base
   validates :serial, :presence => true
-  reverse_geocoded_by :latitude, :longitude, :address => :address
+  reverse_geocoded_by :latitude, :longitude do |obj, result|
+    if geo = result.first
+      obj.address = "#{geo.city}, #{geo.state}, #{geo.country}"
+    end
+  end
   after_validation :reverse_geocode, :if => ->(obj){ obj.latitude.present? && obj.longitude.present? && (obj.latitude_changed? || obj.longitude_changed? || obj.address.blank?) }
 
   def review!
--- spec/support/geocoder.rb
+++ spec/support/geocoder.rb
@@ -6,6 +6,7 @@ Geocoder::Lookup::Test.set_default_stub(
       'latitude'     => 44.56222682932014,
       'longitude'    => -123.26127102380366,
       'address'      => '2nd & Jefferson, Corvallis',
+      'city'         => 'Corvallis',
       'state'        => 'Oregon',
       'state_code'   => 'OR',
       'country'      => 'United States',
