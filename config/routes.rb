4a275cf15acd7d069f3c177fc9dc814551c3869d
luisgreg99 <luisram90_101@hotmail.com>
2015-10-07 10:10:36 -0700

    added new path to register civ cards

--- config/routes.rb
+++ config/routes.rb
@@ -6,13 +6,15 @@ Rails.application.routes.draw do
     end
   end
 
+  get "/entry/:serial", :to => 'civil_entries#register', :as => 'register'
+
   resources :review
 
   get "review_item/:id", :to => 'review#review_card', :as => 'review_card'
   get '/admin', :to => 'admin#index', :as => 'admin_index'
 
   get 'civil_entries/qr/:serial' => Dragonfly.app.endpoint { |params, app|
-    app.generate(:qr, "http://#{Rails.application.config.action_mailer.default_url_options[:host]}/civil_entries/register/#{params[:serial]}")
+    app.generate(:qr, "http://#{Rails.application.config.action_mailer.default_url_options[:host]}/entry/#{params[:serial]}")
   }
 
   root :to => "home#index"
