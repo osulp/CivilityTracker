14884ebc3d12c957836dc485dfbe25dcdc6001b0
luisgreg99 <luisram90_101@hotmail.com>
2015-10-08 10:04:02 -0700

    added spec for about controller

--- spec/controllers/about_controller_spec.rb
+++ spec/controllers/about_controller_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
 
-describe AboutController do
-
+RSpec.describe AboutController do
+  describe "#index" do
+    it "should assign @about_us" do
+      FactoryGirl.create(:setting)
+      get :index
+      expect(assigns(:about_us)).to eq(Setting.about)
+    end
+    it "should render the index template" do
+      get :index
+      # binding.pry
+      expect(response).to render_template("index")
+    end
+  end
 end
