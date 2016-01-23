2fa9faa6d58907aa1525f3c0da1a7e31b0d285e5
luisgreg99 <luisram90_101@hotmail.com>
2016-01-22 17:03:57 -0800

    updated reviewed entries query

--- spec/factories/civil_entries.rb
+++ spec/factories/civil_entries.rb
@@ -4,6 +4,7 @@ FactoryGirl.define do
   factory :civil_entry do
     sequence(:serial) {|n| n}
     reason "This is a reason"
+    address "Corvallis, OR"
     reviewed true
     trait :unreviewed_civil_entry do
       reviewed false
